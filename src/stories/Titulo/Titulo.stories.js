e7451e1ab6f6446ed2d9eed88535c501d0be56b4
SofiaRubiano10 <10sofiarubiano@gmail.com>
2023-06-27 18:37:35 +0200

    Componente Titulo con diferentes Stories y CSS

---
+++ src/stories/Titulo/Titulo.stories.js
@@ -0,0 +1,30 @@
+import Titulo from "./Titulo";
+
+export default {
+  component: Titulo,
+  argTypes: {
+    nivel: {
+      // select, radio, inline-radio
+      control: "inline-radio",
+    },
+  },
+};
+
+export const Header1 = {
+  args: {
+    texto: "Header 1",
+    nivel: 1,
+  },
+};
+export const Header3 = {
+  args: {
+    texto: "Header 3",
+    nivel: 3,
+  },
+};
+export const Header6 = {
+  args: {
+    texto: "Header 6",
+    nivel: 6,
+  },
+};
